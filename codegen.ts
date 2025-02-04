8f7208ee3b9c2488994426dbab91cf4ab3231468
Jason Efstathiou <jason.efst@gmail.com>
2025-02-04 14:23:55 +0100

    feat: docker setup

--- codegen.ts
+++ codegen.ts
@@ -2,15 +2,7 @@ import 'dotenv/config';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: [
-    {
-      [process.env.GRAPHQL_URL]: {
-        headers: {
-          Authorization: `Bearer ${process.env.GRAPHQL_ACCESS_TOKEN}`,
-        },
-      },
-    },
-  ],
+  schema: './schema.graphql',
   generates: {
     './src/application/__generated__/graphql/base-types.ts': {
       plugins: ['typescript', 'typescript-operations'],
